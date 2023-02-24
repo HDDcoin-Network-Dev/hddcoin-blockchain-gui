598ecbfeefe65b9364a89b7d8acdeae2e500e6cb
zcomputerwiz <zcomputerwiz@users.noreply.github.com>
2023-02-24 15:35:43 -0600

    Update HODLterminal.tsx
    Fix missing DashboardTitle, use Typography instead

--- packages/gui/src/components/hodlterminal/HODLterminal.tsx
+++ packages/gui/src/components/hodlterminal/HODLterminal.tsx
@@ -1,25 +1,36 @@
-import * as React from 'react';
-import DashboardTitle from '../dashboard/DashboardTitle';
 import { Flex } from '@chia-network/core';
-import { Paper, Grid } from '@mui/material';
+import { Paper, Grid, Typography } from '@mui/material';
+import * as React from 'react';
+import ScrollToBottom from 'react-scroll-to-bottom';
 import styled from 'styled-components';
-import { Terminal } from "xterm";
-import { FitAddon } from "xterm-addon-fit";
+import { Terminal } from 'xterm';
+import { FitAddon } from 'xterm-addon-fit';
 import 'xterm/css/xterm.css';
-import c from "ansi-colors";
+import c from 'ansi-colors';
+
 import path from 'path';
 import { existsSync, readFileSync } from 'fs';
+
 import { Trans } from '@lingui/macro';
-import ScrollToBottom from 'react-scroll-to-bottom';
+
 const electron = require('electron');
-const clipboard = electron.clipboard;
-							
+
+const { clipboard } = electron;
+
 const PY_MAC_DIST_FOLDER = '../../../app.asar.unpacked/daemon';
 const PY_WIN_DIST_FOLDER = '../../../app.asar.unpacked/daemon';
-const HODL_HELP_PATH = process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'] + '/.chia/mainnet/hodl/hodlhelp.txt';
-const fullPath = (existsSync((process.platform === 'win32') ? path.join(__dirname, PY_WIN_DIST_FOLDER) : path.join(__dirname, PY_MAC_DIST_FOLDER))) ? ((process.platform === 'win32') ? path.join(__dirname, PY_WIN_DIST_FOLDER) : path.join(__dirname, PY_MAC_DIST_FOLDER)) : path.join(__dirname, '../../../venv/bin');
-const ENV_HDDCOIN = ((process.platform === 'win32') ? '$env:Path += ";' : 'export PATH="$PATH:') + fullPath + '"';
-const SHELL = (process.platform === 'win32') ? 'powershell.exe' : 'bash';
+const HODL_HELP_PATH = `${
+  process.env[process.platform === 'win32' ? 'USERPROFILE' : 'HOME']
+}/.chia/mainnet/hodl/hodlhelp.txt`;
+const fullPath = existsSync(
+  process.platform === 'win32' ? path.join(__dirname, PY_WIN_DIST_FOLDER) : path.join(__dirname, PY_MAC_DIST_FOLDER)
+)
+  ? process.platform === 'win32'
+    ? path.join(__dirname, PY_WIN_DIST_FOLDER)
+    : path.join(__dirname, PY_MAC_DIST_FOLDER)
+  : path.join(__dirname, '../../../venv/bin');
+const ENV_HDDCOIN = `${(process.platform === 'win32' ? '$env:Path += ";' : 'export PATH="$PATH:') + fullPath}"`;
+const SHELL = process.platform === 'win32' ? 'powershell.exe' : 'bash';
 const pty = require('node-pty');
 
 // HODL Help / Instructions Stylying
@@ -36,7 +47,7 @@ const StyledPaper = styled(Paper)`
   padding-bottom: 2px;
   padding-left: 2px;
   pre {
-	font-size: 14px;
+    font-size: 14px;
   }
 `;
 
@@ -55,44 +66,64 @@ const fitAddon = new FitAddon();
 const ptyProcess = pty.spawn(SHELL, [], {
   name: 'xterm-color',
   cwd: fullPath,
-  env: process.env
+  env: process.env,
 });
 
 // Set path enviroment
-ptyProcess.write(ENV_HDDCOIN + '\r\n');
+ptyProcess.write(`${ENV_HDDCOIN}\r\n`);
 ptyProcess.write('cd $home \r\n');
 ptyProcess.write('clear \r');
 ptyProcess.write('chia hodl -h\r');
 
 // Write data from ptyProcess to terminal
-ptyProcess.on('data', function(data) {
+ptyProcess.on('data', (data) => {
   term.write(data);
 });
 
 // Get keys
-term.onKey(key => {
+term.onKey((key) => {
   const char = key.domEvent.key;
-  if (char === "Enter") {
+  if (char === 'Enter') {
     ptyProcess.write('\r');
-  } else if (char === "Backspace") {
+  } else if (char === 'Backspace') {
     ptyProcess.write('\b');
-  }  else if (char === "Tab") {
+  } else if (char === 'Tab') {
     ptyProcess.write('\t');
-  } else if (char === "ArrowUp") {
-    ptyProcess.write('\x1b[A')
-  } else if (char === "ArrowDown") {
-    ptyProcess.write('\x1b[B')
-  } else if (char === "ArrowRight") {
-    ptyProcess.write('\x1b[C')
-  } else if (char === "ArrowLeft") {
-    ptyProcess.write('\x1b[D')
-  } else if (char === "Delete" || char === "Insert" || char === "Home" || char === "End" || char === "PageUp" || char === "PageDown" || char === "Escape" || char === "F1" || char === "F2" || char === "F3" || char === "F4" || char === "F5" || char === "F6" || char === "F7" || char === "F8" || char === "F9" || char === "F10" || char === "F11" || char === "F12") {
-    ptyProcess.write('')
-  } else if (term.hasSelection() && key.domEvent.ctrlKey && key.domEvent.key === "c") {
-    clipboard.writeText(term.getSelected())
-  } else if (key.domEvent.ctrlKey && key.domEvent.key === "v") {
+  } else if (char === 'ArrowUp') {
+    ptyProcess.write('\x1b[A');
+  } else if (char === 'ArrowDown') {
+    ptyProcess.write('\x1b[B');
+  } else if (char === 'ArrowRight') {
+    ptyProcess.write('\x1b[C');
+  } else if (char === 'ArrowLeft') {
+    ptyProcess.write('\x1b[D');
+  } else if (
+    char === 'Delete' ||
+    char === 'Insert' ||
+    char === 'Home' ||
+    char === 'End' ||
+    char === 'PageUp' ||
+    char === 'PageDown' ||
+    char === 'Escape' ||
+    char === 'F1' ||
+    char === 'F2' ||
+    char === 'F3' ||
+    char === 'F4' ||
+    char === 'F5' ||
+    char === 'F6' ||
+    char === 'F7' ||
+    char === 'F8' ||
+    char === 'F9' ||
+    char === 'F10' ||
+    char === 'F11' ||
+    char === 'F12'
+  ) {
+    ptyProcess.write('');
+  } else if (term.hasSelection() && key.domEvent.ctrlKey && key.domEvent.key === 'c') {
+    clipboard.writeText(term.getSelected());
+  } else if (key.domEvent.ctrlKey && key.domEvent.key === 'v') {
     term.focus();
-    ptyProcess.write(clipboard.readText())
+    ptyProcess.write(clipboard.readText());
   } else {
     ptyProcess.write(char);
   }
@@ -100,60 +131,56 @@ term.onKey(key => {
 
 export default class HODLterminal extends React.Component {
   constructor(props) {
-    super(props);	
-	this.state = {
-      hodlhelp: "Loading HODL Help / Instructions...",
-	};
+    super(props);
+    this.state = {
+      hodlhelp: 'Loading HODL Help / Instructions...',
+    };
   }
-  
+
   // Read content of hodlhelp.txt
   checkHODLhelpFile() {
-
     if (existsSync(HODL_HELP_PATH)) {
       const buffer = readFileSync(HODL_HELP_PATH);
       const hodlhelpContent = buffer.toString();
-      this.setState({hodlhelp: hodlhelpContent});
+      this.setState({ hodlhelp: hodlhelpContent });
     }
   }
-  
+
   componentDidMount() {
-	  
-	// Load HODL Help / Instructions
-	this.checkHODLhelpFile();
-	 
-	// Load the Fit Addon and open the Terminal in #xterm terminal-container
-	term.loadAddon(fitAddon);
-	term.open(document.getElementById('xterm'));
-  
+    // Load HODL Help / Instructions
+    this.checkHODLhelpFile();
+
+    // Load the Fit Addon and open the Terminal in #xterm terminal-container
+    term.loadAddon(fitAddon);
+    term.open(document.getElementById('xterm'));
+
     // Make the terminal's size and geometry fit the size of terminal-container
-    term.onResize(params => {
+    term.onResize((params) => {
       ptyProcess.resize(params.cols, params.rows);
     });
-	
-	fitAddon.fit();
+
+    fitAddon.fit();
   }
-  
+
   // Display HODL Terminal and Help / Instructions in the GUI
   render() {
     return (
       <Flex flexDirection="column" flexGrow="1">
+        <Typography variant="h5" gutterBottom>
+          <Trans>HDDcoin HODL and Apps Terminal</Trans>
+        </Typography>
 
-        <DashboardTitle>
-            <Trans>HDDcoin HODL and Apps Terminal</Trans>
-        </DashboardTitle>
-		
-		<Grid container alignItems="stretch">	   
-			<Flex flexDirection="column" flexGrow="1" alignItems="center">
-				<div id="xterm" style={{ height: "55vh", width: "100%" }} />					
-			</Flex>		  
-		</Grid>
-		
-		<StyledPaper>
-			<StyledScrollToBottom>
-				<pre>{this.state.hodlhelp}</pre>
-			</StyledScrollToBottom>
+        <Grid container alignItems="stretch">
+          <Flex flexDirection="column" flexGrow="1" alignItems="center">
+            <div id="xterm" style={{ height: '55vh', width: '100%' }} />
+          </Flex>
+        </Grid>
+
+        <StyledPaper>
+          <StyledScrollToBottom>
+            <pre>{this.state.hodlhelp}</pre>
+          </StyledScrollToBottom>
         </StyledPaper>
-				
       </Flex>
     );
   }
